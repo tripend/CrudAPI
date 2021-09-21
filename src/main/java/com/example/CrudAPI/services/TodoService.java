ec88d62746ef314918f428fc8330b16988dcd3bb
tripend <arjunnkamalbatra@gmail.com>
2021-09-21 15:27:11 -0500

    Created Todo Service

---
+++ src/main/java/com/example/CrudAPI/services/TodoService.java
@@ -0,0 +1,17 @@
+package com.example.CrudAPI.services;
+
+import com.example.CrudAPI.Todo;
+import java.util.List;
+
+
+public interface TodoService {
+    List<Todo> getTodos();
+
+    Todo getTodoById(Long id);
+
+    Todo insert(Todo todo);
+
+    void updateTodo(Long id, Todo todo);
+
+    void deleteTodo(Long todoId);
+}
